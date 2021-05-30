05b79345cb2bd177691009aae892c943182545b9
Herval Mata <hervalmataamparo@gmail.com>
2021-05-30 13:43:20 -0300

    feat:art: add next pwa

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,8 +7,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png"/>
-        <link rel="apple-touch-icon" href="/img/icon-512.png"/>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="Client to won games"
