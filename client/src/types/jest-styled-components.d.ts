8d1120a21e390288648567fc4f2cacc6029b663c
Herval Mata <hervalmataamparo@gmail.com>
2021-05-30 15:48:13 -0300

    feat:art: fix jest-dom-matchers

---
+++ client/src/types/jest-styled-components.d.ts
@@ -0,0 +1,21 @@
+import { Plugin, NewPlugin} from 'pretty-format'
+
+declare global {
+  namespace jest {
+    interface AsymmetricMatcher {
+      $$typeof: symbol
+      sample?: string | RegExp | object | Array<any> | Function
+    }
+    type Value = string | RegExp | object | AsymmetricMatcher | undefined
+    interface Options {
+      media?: string
+      modifier?: string
+      supports?: string
+    }
+    interface Matchers<R, T> {
+      toHaveStyleRule(property: string, value?: Value, options?: Options): R
+    }
+  }
+}
+
+export declare const styleSheetSerializer: Exclude<Plugin, NewPlugin>
