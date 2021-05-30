3a7e962fca28db8ac41c3fb689b67484b6281678
Herval Mata <hervalmataamparo@gmail.com>
2021-05-29 22:18:01 -0300

    feat:art: add global style an custom _app

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import {AppProps} from "next/app";
+import Head from "next/head";
+import GlobalStyles from "../styles/global";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Won Games</title>
+        <link rel="shortcut icon" href="/img/icon-512.png"/>
+        <link rel="apple-touch-icon" href="/img/icon-512.png"/>
+        <meta
+          name="description"
+          content="Client to won games"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+
+}
