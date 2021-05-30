1da9015b76b61a2b8ddbe8a5f276a49bcc948278
Herval Mata <hervalmataamparo@gmail.com>
2021-05-30 16:00:51 -0300

    feat:art: add features

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
           content="Client to won games"
